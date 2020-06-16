fda48def0f6fb84efaf8691e140e4685d4101940
sashanksilwal <sashanksilwal@hotmail.com>
2020-06-17 01:21:52 +0400

    added ending

--- js/script.js
+++ js/script.js
@@ -63,6 +63,7 @@ itemFrame4.addEventListener("click",()=>{
     $("#item4").fadeOut(4000);
      bg_image.style.background = "url('white.png')"; 
     Frame_mp3.play();
+    setTimeout(() =>{typewriter();}, 85000)
 
 })
 
@@ -204,3 +205,50 @@ function openFAB(event) {
     overlay.removeClass('dark-overlay');
   }
   
+
+  // set up text to print, each item in array is new line
+var aText = new Array(
+    "To live in this world",
+    "you must be able",
+    "to do three things:",
+    "to love what is mortal;",
+    "to hold it",
+    "against your bones knowing",
+    "your own life depends on it;",
+    "and, when the time comes to let it go,",
+    "to let it go"
+
+    );
+    var iSpeed = 100; // time delay of print out
+    var iIndex = 0; // start printing array at this posision
+    var iArrLength = aText[0].length; // the length of the text array
+    var iScrollAt = 20; // start scrolling up at this many lines
+     
+    var iTextPos = 0; // initialise text position
+    var sContents = ''; // initialise contents variable
+    var iRow; // initialise current row
+     
+    function typewriter()
+    {
+     sContents =  ' ';
+     iRow = Math.max(0, iIndex-iScrollAt);
+     var destination = document.getElementById("typedtext");
+     
+     while ( iRow < iIndex ) {
+      sContents += aText[iRow++] + '<br />';
+     }
+     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+     if ( iTextPos++ == iArrLength ) {
+      iTextPos = 0;
+      iIndex++;
+      if ( iIndex != aText.length ) {
+       iArrLength = aText[iIndex].length;
+       setTimeout("typewriter()", 500);
+      }
+     } else {
+      setTimeout("typewriter()", iSpeed);
+     }
+    }
+    
+    
+   
