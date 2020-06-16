caae2b173b51995402802be477837904e2e58197
sashanksilwal <sashanksilwal@hotmail.com>
2020-06-16 18:59:01 +0400

    Edited the main page

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 let box = document.getElementById("box");
 let bg_image = document.getElementById("bg_image");
 let Footsteps_mp3 = document.getElementById("Footsteps");
-let pauseBtn = document.getElementById("pause");
+let pauseBtn = document.getElementById("pausebtn");
 let main_page=false;
 let second_page=false;
 let items= ["item1", "item2", "item3","item4"]
 
 
 box.addEventListener("click",()=>{
+    
+    $("#infodiv").fadeOut(500);
+    $('#pausebtn').fadeIn(500); 
     main_page = true;
     box.style.display="none";
     bg_image.style.background = "url('black.png')"; 
@@ -28,8 +31,39 @@ function myFunction() {
     second_page=true;
     console.log("hello you have ended the sound");
     bg_image.setAttribute("class", "bg_image_second");
-    bg_image.style.background = "url('../media/images/inside_a_box.jpg')"; 
+    bg_image.style.background = "url('media/images/inside_a_box.jpg')"; 
     for (let i=0; i<items.length-1; i++){
-        document.getElementById(items[i]).setAttribute("class", "item ", items[i] ," item_display ");
+        document.getElementById(items[i]).setAttribute("class", "item "+items[i]+" item_display ");
     }
+}
+
+
+
+
+var overlay = $("#overlay"),
+        fab = $(".fab"),
+     cancel = $("#cancel"),
+     submit = $("#submit");
+
+//fab click
+fab.on('click', openFAB);
+overlay.on('click', closeFAB);
+cancel.on('click', closeFAB);
+
+function openFAB(event) {
+  if (event) event.preventDefault();
+  fab.addClass('active');
+  overlay.addClass('dark-overlay');
+
+}
+
+function closeFAB(event) {
+  if (event) {
+    event.preventDefault();
+    event.stopImmediatePropagation();
+  }
+
+  fab.removeClass('active');
+  overlay.removeClass('dark-overlay');
+  
 }
